1b90a85b46517a7e5a3413991c9d359f42f3a8da
saSaimon <asadiqulsaimon@gmail.com>
2021-10-20 00:10:57 +0600

    main

---
+++ project2js/project.js
@@ -0,0 +1,18 @@
+
+
+for(var i=0; i<=100; i++){
+    if(i%3==0){
+        console.log("Jesse")
+    } else if (i%5==0)
+    {
+        console.group("James")
+    } else if(i%3==0 && i%5==0){
+        console.log("Team Rocket")
+    } else
+    {
+        console.log(i)
+    }
+    
+    }
+
+
